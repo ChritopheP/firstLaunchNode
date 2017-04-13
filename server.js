c3130a76ff4f5f02e0a7662871ac91f1e8c73db5
ChristopheP <pereira.christophe1995@gmail.com>
2017-04-13 14:30:31 +0200

    marche

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
-var http = require('http');
+// var http = require('http');
+//
+// var ip = '127.0.0.1';
+// var port = 12107;
+//
+// var server = http.createServer(
+//     function(request,response){
+//         response.statusCode = 200;
+//         response.setHeader('Content-Type','text/plain');
+//         response.end('bisous');
+//     }
+//
+// );
+//
+// server.listen(port,ip,function() {
+//   console.log('le serveur tourne bien');
+//
+// });
 
-var ip = '127.0.0.1';
-var port = 12107;
+var express = require('express');
+var serveur = express();
 
-var server = http.createServer(
-    function(request,response){
-        response.statusCode = 200;
-        response.setHeader('Content-Type','text/plain');
-        response.end('bisous');
-    }
-
-);
-
-server.listen(port,ip,function() {
-  console.log('le serveur tourne bien');
-
+serveur.get("/",function(request,response){
+  response.sendFile (__dirname + "/views/index.html");
 });
+serveur.get("/toto",function(request,response){
+  response.send ("Bisous de toto");
+});
+serveur.listen(12107);
